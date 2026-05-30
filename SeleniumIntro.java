f133398276aa04cc8f6679900efbf7d7aa7ced2d
Johanz211 <johannes112110@gmail.com>
2026-05-30 20:40:21 +0530

    Cheers to new beginning

---
+++ SeleniumIntro.java
@@ -0,0 +1,10 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class SeleniumIntro
+{
+    public static void main(String[] args) {
+        WebDriver safariDriver = new SafariDriver();
+        safariDriver.get("https://www.google.com");
+    }
+}
